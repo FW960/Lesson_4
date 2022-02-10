383ba06f924a7108f79fb500a9ce8f94594b7a67
FW960 <artuninp@gmail.com>
2022-02-10 21:12:28 +0800

    Числа Фибоначчи

--- Lesson_4/Lesson_4/Program.cs
+++ Lesson_4/Lesson_4/Program.cs
@@ -6,7 +6,33 @@ namespace Lesson_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long[] maxValue = new long[93];
+
+            int fibNum1 = 0;
+
+            int fibNum2 = 1;
+
+            maxValue[0] = fibNum1;
+
+            maxValue[1] = fibNum2;
+
+            for (int i = 1; i < maxValue.Length - 1; i++)
+            {
+
+                maxValue[i + 1] = maxValue[i - 1] + maxValue[i];
+
+                if (i == maxValue.Length)
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine("Введите число, чтобы преобразовать его в число Фибоначчи от 0 до 92: ");
+
+            int fibNum3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(maxValue[fibNum3]);
+
         }
     }
 }
